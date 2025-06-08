4b916e9aa5a10d3769a4976fe9ad55a20edcccc0
YCMM16022000 <yourcarlosjr@gmail.com>
2025-06-08 21:31:48 +0200

    Consultas medicas tests

---
+++ consultas_medicas/src/test/java/com/consultas/consultas_medicas/ConsultasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultas.consultas_medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
