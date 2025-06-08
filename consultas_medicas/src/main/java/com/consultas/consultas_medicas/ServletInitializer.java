a0fe3da6eda9b8e8b5e3f621d3ba77e265ff6bd3
HenriquesPanhela <henriquesmercio18@gmail.com>
2025-06-08 21:10:14 +0200

    Servlet

---
+++ consultas_medicas/src/main/java/com/consultas/consultas_medicas/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.consultas.consultas_medicas;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConsultasMedicasApplication.class);
+	}
+
+}
